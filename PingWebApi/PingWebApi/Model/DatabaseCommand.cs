def9c382660a07722493bcefb224d36fcf94246e
Emil Hammer <mr_h4mm3r@live.dk>
2019-05-20 12:35:34 +0200

    Fix

--- PingWebApi/PingWebApi/Model/DatabaseCommand.cs
+++ PingWebApi/PingWebApi/Model/DatabaseCommand.cs
@@ -69,7 +69,7 @@ namespace PingWebApi.Model
 
                 foreach (var unused in dataReader)
                 {
-                    UserScore score = new UserScore((string)dataReader.GetValue(0), (int)dataReader.GetValue(1));
+                    UserScore score = new UserScore((string)dataReader.GetValue(0), (int)dataReader.GetValue(1),(string)dataReader.GetValue(2));
                     list.Add(score);
                 }
                 return list;
